f45d34a6dcac008cadbe0151c251ae46a8493f24
santiago hurtado <sahurtaro@gmail.com>
2025-01-15 15:12:59 -0500

    jwt added into login service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { UsersService } from '../users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants/jwt.constant';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,14 @@ import {
 import { RegisterDto } from './dto/register.dto';
 import * as bcryptjs from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
   async register(registerDto: RegisterDto) {
     const user = await this.usersService.findOneByEmail(registerDto.email);
 
@@ -36,6 +40,10 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new UnauthorizedException('Password is wrong');
     }
-    return user;
+
+    const payload = { email: user.email };
+
+    const token = await this.jwtService.signAsync(payload);
+    return { token, email: user.email };
   }
 }
